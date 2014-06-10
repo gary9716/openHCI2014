960258d8fab0f23ca58b5d621656212f3e4494b4
tsuKiNo <gary9716@gmail.com>
2014-06-10 22:27:33 +0800

    make it runnable on heroku

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ var s3_username = process.env.AWS_S3_USER;
 var s3_password = process.env.AWS_S3_PASS;
 var s3_bucket = process.env.S3_BUCKET_NAME;
 var s3_isAvailable;
+var s3_client;
 
 if(!s3_access_key_id || !s3_secret_access_key || !s3_username || !s3_password || !s3_bucket) {
 	s3_isAvailable = false;
@@ -39,27 +40,46 @@ if(!s3_access_key_id || !s3_secret_access_key || !s3_username || !s3_password ||
 else {
 	s3_isAvailable = true;
 	form.uploadDir = __dirname + '/tmp';
+	s3_client = require('knox').createClient({
+	    key: s3_access_key_id
+	  , secret: s3_secret_access_key
+	  , bucket: s3_bucket
+	});
+
 }
 
 var global_res;
 
 form.on('file', function(field, file) {
         //rename the incoming file to the file's local name
-        fs.rename(file.path, form.uploadDir + "/" + file.name,function (err) {
+        var newFileNameAndPath = form.uploadDir + "/" + file.name;
+        fs.rename(file.path, newFileNameAndPath,function (err) {
 			console.log('file rename to:' + file.name);
-			try {
-				if(err) {
-	        		console.log(err);
-	        		global_res.send(400, { error: 'file already there' }); //400 means bad request
-	        	}
-	        	else {
-	        		console.log("no error");
-	        		global_res.end("upload complete");
-	        	}
+			
+			// if(!err) {
+			// 	s3_client.putFile(newFileNameAndPath, file.name, function(err, res){
+			// 	  // Always either do something with `res` or at least call `res.resume()`.
+					
+			// 		res.resume();
+			// 		if(!err) {
+			// 			global_res.end("upload complete");
+			// 		}
+			// 		else {
+			// 			global_res.send(400, { error: 'upload failed' }); //400 means bad request
+			// 		}
+			// 	});
+			// }
+
+
+			if(err) {
+        		console.log(err);
+        		global_res.send(400, { error: 'upload failed' }); //400 means bad request
         	}
-        	catch(e) {
-        		console.log(e);
+        	else {
+        		console.log("no error");
+        		global_res.end("upload complete");
         	}
+      	
         });
     })
 	.on('end', function() {
@@ -67,13 +87,10 @@ form.on('file', function(field, file) {
 		return;
 	});
 
-app.get("/uploadFile",function (req,res) {
-	if(!s3_isAvailable) {
-		res.sendfile(uploadFileHtmlPath);
-	}
-	else { //when s3 is available
 
-	}
+//http routing
+app.get("/uploadFile",function (req,res) {
+	res.sendfile(uploadFileHtmlPath);
 })
 .get("/file/download/:fileName",function (req,res) {
 	if(!s3_isAvailable) {
@@ -87,7 +104,10 @@ app.get("/uploadFile",function (req,res) {
 		);
 	}
 	else { 
-
+		s3_client.getFile(req.params.fileName, function (err,res) {
+			console.log(res);
+			res.resume();
+		});
 	}
 })
 .post("/file/upload",function (req,res) {
